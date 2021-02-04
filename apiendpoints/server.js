042f3483caa5d0d3e79d72ecd6db82ae6821e698
daproclaima <sn.itpro@gmail.com>
2021-02-04 23:37:06 +0100

    feat(middleware): add error and not found Handlers

--- apiendpoints/server.js
+++ apiendpoints/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 dotenv.config()
 connectDB()
 
@@ -11,11 +11,19 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const env = process.env.NODE_ENV || 'development'
 
+// app.use((req, res, next) => {
+//     console.log(req.originalUrl)
+//     next()
+// })
+
 app.get('/', (req   , res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT,
     console.log(
