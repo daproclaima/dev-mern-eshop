fb7a9f20c6e5b9d7370a7725d9c3eee906d84b76
daproclaima <sn.itpro@gmail.com>
2021-02-04 21:36:04 +0100

    chore(seeder.js): add functions to persist and delete data in db in package script

---
+++ apiendpoints/seeder.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+dotenv.config()
+
+connectDB()
+
+// execute these functions by adding them in package.json
+// "data:import": "node ./apiendpoints/seeder",
+// "data:destroy": "node ./apiendpoints/seeder -d"
+
+const importData = async () => {
+    try {
+        await Order.deleteMany()
+        await User.deleteMany()
+        await Product.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+
+        const adminUserId = createdUsers[0]._id
+
+        const sampleProducts = products.map(product => {
+            return {...product, user: adminUserId}
+        })
+        console.log('Data to import')
+        console.log(JSON.parse(JSON.stringify(sampleProducts)))
+
+        await Product.insertMany(sampleProducts)
+
+        console.log('Data imported'.green.inverse)
+        process.exit()
+    } catch (e) {
+        console.log(`${e}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await Order.deleteMany()
+        await User.deleteMany()
+        await Product.deleteMany()
+
+        console.log('Data destroyed'.yellow.inverse)
+        process.exit()
+    } catch (e) {
+        console.log(`${e}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+if (process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
+}
