11c80bf6c7224a3a4c9ec00cc9810629a1318c0c
daproclaima <sn.itpro@gmail.com>
2021-02-05 10:32:01 +0100

    feat: loads productList in homescreen through store and handle error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,36 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import axios from "axios";
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Row, Col} from 'react-bootstrap'
 
+import Product from '../components/Product'
+import {listProducts} from '../actions/productActions'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('api/products')
-      setProducts(data)
-    }
+    const dispatch = useDispatch()
 
-    fetchProducts()
-  }, [])
+    // productList
+    const {loading, error, products} = useSelector(state => state.productList)
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
 
-  return (
-    <>
-    <h1>Latest products</h1>
-    <Row>
+    return (
+        <>
+            <h1>Latest products</h1>
+            {
+                loading
+                    ? <h2>Loading...</h2>
+                    : error ? <h2>{error}</h2>
+                    : <Row>
 
-        {products.map(product => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-        ))}
-    </Row>
-
-
-    </>
-  )
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product}/>
+                            </Col>
+                        ))}
+                    </Row>}
+        </>
+    )
 }
 
 export default HomeScreen
