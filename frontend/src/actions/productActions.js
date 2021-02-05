83c5862a468a32503ea5b972fc95eee23d75f6a0
daproclaima <sn.itpro@gmail.com>
2021-02-05 19:05:52 +0100

    feat(productDetails): add productDetails to store with its reducer and action and adapt ProductScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,12 +3,15 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("api/products");
+    const { data } = await axios.get("/api/products");
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
@@ -25,4 +28,22 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-export default listProducts;
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 
 import Product from "../components/Product.jsx";
-import listProducts from "../actions/productActions";
+import { listProducts } from "../actions/productActions";
 
 import Message from "../components/Message";
 import Loader from "../components/Loader";
