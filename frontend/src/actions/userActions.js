8cc2095a08449784cafc957219a702377f1391e4
daproclaima <sn.itpro@gmail.com>
2021-02-06 16:12:36 +0100

    feat(userActions): add actions to login/logout user

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,43 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+import axios from "axios";
+import { PRODUCT_DETAILS_FAIL } from "../constants/productConstants";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    // TODO website name
+    localStorage.setItem("userInfoMERNESHOP", JSON.stringify(data));
+  } catch (e) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
