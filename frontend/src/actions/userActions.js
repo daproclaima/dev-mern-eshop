673f8f9995ffe1fe69927aa6484f3ad89bd7c6ce
daproclaima <sn.itpro@gmail.com>
2021-02-06 18:17:13 +0100

    feat(actions): add user actions and constants

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,11 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
-import { PRODUCT_DETAILS_FAIL } from "../constants/productConstants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -30,8 +32,7 @@ export const login = (email, password) => async (dispatch) => {
       payload: data,
     });
 
-    // TODO website name
-    localStorage.setItem("userInfoMERNESHOP", JSON.stringify(data));
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (e) {
     dispatch({
       type: USER_LOGIN_FAIL,
@@ -44,6 +45,46 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 export const logout = () => (dispatch) => {
-  localStorage.removeItem("userInfoMERNESHOP");
+  localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (e) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
 };
