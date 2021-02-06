aad9014e10d268fe8f51b6328bb4e8423c43703d
daproclaima <sn.itpro@gmail.com>
2021-02-06 10:18:40 +0100

    feat(server.js): add routes for users

--- apiendpoints/server.js
+++ apiendpoints/server.js
@@ -2,7 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+
+// Routes
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+// Middlewares
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -17,11 +22,14 @@ const env = process.env.NODE_ENV || "development";
 //     next()
 // })
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
