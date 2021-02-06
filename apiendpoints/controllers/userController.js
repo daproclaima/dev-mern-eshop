bf6649b095effda038c57380fa217d6feebf92f0
daproclaima <sn.itpro@gmail.com>
2021-02-06 11:25:28 +0100

    feat(auth): add auth middleware, protects private routes

--- apiendpoints/controllers/userController.js
+++ apiendpoints/controllers/userController.js
@@ -5,7 +5,7 @@ import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
 // @desc Auth users & get token
-// @route POST /api/user/login
+// @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -26,4 +26,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile };
