ec78fb0809627ce70d68dce27f77f5a7b8f96409
daproclaima <sn.itpro@gmail.com>
2021-02-08 19:14:52 +0100

    feat(userActions): rename getUsers to listUser

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -172,7 +172,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   }
 };
 
-export const getUsers = () => async (dispatch, getState) => {
+export const listUsers = () => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_LIST_REQUEST,
