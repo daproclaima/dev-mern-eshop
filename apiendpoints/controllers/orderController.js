b88720beddb88bf90b35fd19bc9facf64456f8fa
daproclaima <sn.itpro@gmail.com>
2021-02-08 16:11:49 +0100

    fix(order controller): fix error message for not found

--- apiendpoints/controllers/orderController.js
+++ apiendpoints/controllers/orderController.js
@@ -17,7 +17,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
   if (orderItems && orderItems.length === 0) {
     res.status(400);
-    throw new Error("New order items");
+    throw new Error("No order items");
   } else {
     const order = new Order({
       orderItems,
