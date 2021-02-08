138c2ac3ab69e34a8f77916ee57b8c65a0b14ee5
daproclaima <sn.itpro@gmail.com>
2021-02-08 19:03:20 +0100

    feat(userActions): add getUsers action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,6 +13,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
@@ -161,6 +164,39 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   } catch (e) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
+
+export const getUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/users`, config);
+
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: USER_LIST_FAIL,
       payload:
         e.response && e.response.data.message
           ? e.response.data.message
