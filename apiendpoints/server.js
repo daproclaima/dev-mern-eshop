27ad14a8057e7bc00fb259e3be41664bb958fe77
daproclaima <sn.itpro@gmail.com>
2021-02-22 22:59:34 +0100

    core(server): add use of morgan http logger

--- apiendpoints/server.js
+++ apiendpoints/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import path from "path";
 import connectDB from "./config/db.js";
 
@@ -17,6 +18,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 const PORT = process.env.PORT || 5000;
 const env = process.env.NODE_ENV || "development";
 
