d1a8d8373ec357c3566609462f4ee6ecba13a4c3
daproclaima <sn.itpro@gmail.com>
2021-02-23 20:22:23 +0100

    feat(product): add createdAt field to reviewSchema

--- apiendpoints/models/productModel.js
+++ apiendpoints/models/productModel.js
@@ -5,6 +5,9 @@ const reviewSchema = mongoose.Schema(
     name: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
+    createdAt: {
+      type: Date,
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
