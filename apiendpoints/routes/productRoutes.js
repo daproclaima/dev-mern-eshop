fd4526f7895ef1916bf600cab26c6917e28285f3
daproclaima <sn.itpro@gmail.com>
2021-02-24 07:46:25 +0100

    feat(products): add get top rated products endpoint

--- apiendpoints/routes/productRoutes.js
+++ apiendpoints/routes/productRoutes.js
@@ -6,12 +6,15 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+
+router.route("/top").get(getTopProducts);
 
 router
   .route("/:id")
