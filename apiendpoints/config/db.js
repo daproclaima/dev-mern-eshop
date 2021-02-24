e36aa9f97cddec1f10c341fce66b0011bea92d6c
daproclaima <sn.itpro@gmail.com>
2021-02-24 21:44:37 +0100

    refactor: remove logs

--- apiendpoints/config/db.js
+++ apiendpoints/config/db.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  console.log(process.env.MONGO_URI);
   try {
+    // eslint-disable-next-line no-unused-vars
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+    // console.log(process.env.MONGO_URI);
+    // console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (e) {
     console.error(`Error: ${e.message}`.red.bold);
     process.exit(1);
